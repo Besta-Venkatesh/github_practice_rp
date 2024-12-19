bb3bce6a15ade5c99376f53d68549c243654e810
venky <venky1998.31@gmail.com>
2024-12-19 11:51:53 +0530

    created new pandas  files

--- explorepy/Python_explore/New_explore_Py/pandas_shuru.py
+++ explorepy/Python_explore/New_explore_Py/pandas_shuru.py
@@ -23,6 +23,7 @@ dbname = 'zishta2024dump'
 port = 5432
 user = 'postgres'
 password = 'log'
+schema = 'zishta2024'
 
 connections  = pg2.connect(
     host = hostname,
@@ -31,8 +32,18 @@ connections  = pg2.connect(
     password = password
 )
 ref_cursor  = connections.cursor()
-ref_cursor.execute("select i.itemid,i.itemdesc,i.itemcode,i.mrp from zishta2024.item i")
+ref_cursor.execute(f"select i.itemid,i.itemdesc,i.itemcode,i.mrp from {schema}.item i")
 rows = ref_cursor.fetchall()
 columns = [desc[0] for desc in ref_cursor.description]
 df = pd.DataFrame(rows,columns=columns)
-print(df)
+
+ref_cursor.execute(f'select l.* from {schema}.branch b join {schema}.location l on b.branchid = l.branch')
+loc_columns = [ desc[0] for desc in ref_cursor.description]
+all_rows = ref_cursor.fetchall()
+loc_branch = pd.DataFrame(all_rows,columns =loc_columns )
+
+loc_branch.to_json('loc_branch.json', orient='split')
+loc_branch.to_json('loc_branch_columns',orient='columns')
+loc_branch.to_json('loc_branch_index',orient='index')
+loc_branch.to_json('loc_branch_records',orient='records')
+loc_branch.to_json('loc_branch_values',orient='values')
